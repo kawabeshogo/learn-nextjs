b0372c3b7c7de2c7db2218c869abd3679fc9333d
kawabeshogo <shogo.kawabe@furukawaelectric.com>
2024-02-18 22:54:35 +0900

    chapter4-6 finished

--- nextjs-dashboard/prettier.config.js
+++ nextjs-dashboard/prettier.config.js
@@ -1,6 +1,14 @@
-const styleguide = require('@vercel/style-guide/prettier');
-
+/** @type {import('prettier').Config} */
 module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
+  semi: false, // セミコロン無し
+  singleQuote: true, // シングルクォート使う
+  printWidth: 90, // 折り返し
+  tabWidth: 2, // スペースの単位
+  trailingComma: 'all', // ケツカンマつける
+  jsxSingleQuote: true, //　JSXでシングルクォートを使用
+  tailwindcss: {
+    tailwindFunctions: ['clsx'],
+  },
+  // ...styleguide,
+  // plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+}
